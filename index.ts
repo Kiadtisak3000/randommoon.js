a30686b7b3fb45caecabd0965dd302e3ca4f29e2
nonahaya <nonahaya68@gmail.com>
2024-08-24 12:28:09 +0700

    Add Index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { rand16 } from "./randomU16";
+import { randarr } from "./lib/randarr";
+import { randuni } from "./randuni";
+import { Zeros } from "./zeros";
+export default {
+  rand16,
+  randarr,
+  randuni,
+  Zeros,
+};
+export { rand16, randarr, randuni, Zeros };
