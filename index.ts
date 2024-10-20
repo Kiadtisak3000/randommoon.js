0318301fb6a4e04ddbbd675b4fff48c35dffc1f5
kiadtisak3000 <kiadtisak3000@gmail.com>
2024-10-20 12:04:55 +0700

    Revert "Add linspace"
    This reverts commit 1b4b26034e98f2221912569fdc0e6d423eac3c1b.

--- index.ts
+++ index.ts
@@ -3,13 +3,11 @@ import { randarr } from "./lib/randarr";
 import { randuni } from "./randuni";
 import { Zeros } from "./zeros";
 import { randNorm } from "./randnorm";
-import { Linspace } from "./linspace";
 export default {
   rand16,
   randarr,
   randuni,
   Zeros,
-  randNorm,
-  Linspace
+  randNorm
 };
-export { rand16, randarr, randuni, Zeros,randNorm,Linspace };
+export { rand16, randarr, randuni, Zeros,randNorm };
