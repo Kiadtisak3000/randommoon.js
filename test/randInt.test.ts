a417affc45a6a13bcc91f828f08ebf7d3ff681dd
kiadtisak3000 <kiadtisak3000@gmail.com>
2024-11-14 10:40:16 +0700

    fix test

--- test/randInt.test.ts
+++ test/randInt.test.ts
@@ -3,14 +3,14 @@ import { randInt } from "../randInt";
 describe("Random Integer Test",() =>{
     test("random test",() =>{
         expect(randInt(5).data).toBeLessThanOrEqual(5)
-        expect(randInt([9]).data).toBeLessThanOrEqual(9)
+        expect(randInt(9).data).toBeLessThanOrEqual(9)
     })
     test("specific random test",() =>{
-        expect(randInt([5,10]).data).toBeLessThanOrEqual(10)
+        expect(randInt(5,10).data).toBeLessThanOrEqual(10)
     })
     test("Fail Test",() =>{
         try{
-            randInt([10,5])
+            randInt(10,5)
             throw new Error("Test Fail!")
         }catch(e:any){expect(e.message).toBe("Range value must more then base value")}
     })
